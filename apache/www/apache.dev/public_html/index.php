8eb10dfa52c34e13f693ac0aa309e4ab9dba22fc
mao <green.turban@gmail.com>
2016-03-02 18:38:31 +0200

    Apache virtual hosts work. Dependant on host's Acrylic DNS cache

---
+++ apache/www/apache.dev/public_html/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $_SERVER['SCRIPT_FILENAME'] ?></title>
+</head>
+<body>
+	<h1><?php echo $_SERVER['DOCUMENT_ROOT'] ?></h1>
+</body>
+</html>
